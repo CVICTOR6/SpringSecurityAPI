c58eb33bbb59c7c744ab19fc08b5acae525adb0d
CVICTOR6 <105362876+CVICTOR6@users.noreply.github.com>
2023-05-03 14:28:02 +0100

    Initial commit

---
+++ src/main/java/com/cvictor/springsecurity/auth/AuthenticationService.java
@@ -0,0 +1,48 @@
+package com.cvictor.springsecurity.auth;
+
+import com.cvictor.springsecurity.config.JwtService;
+import com.cvictor.springsecurity.user.Role;
+import com.cvictor.springsecurity.user.User;
+import com.cvictor.springsecurity.user.UserRepository;
+import com.fasterxml.jackson.core.Base64Variant;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticateRequest request) {
+       authenticationManager.authenticate(
+           new UsernamePasswordAuthenticationToken(
+                   request.getEmail(),
+                   request.getPassword()
+           )
+       );
+       var user = repository.findByEmail(request.getEmail())
+               .orElseThrow();
+        return null;
+    }
+}
---
+++ src/main/java/com/cvictor/springsecurity/user/Role.java
@@ -0,0 +1,7 @@
+package com.cvictor.springsecurity.user;
+
+public enum Role {
+
+    USER,
+    ADMIN
+}
